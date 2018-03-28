70c2b63de545ce16f382b549b0f90a60e3d9b369
Xcode User <xcode@Fasts-MacBook-Pro.local>
2018-03-27 23:01:21 -0400

    Setup View Controller Layout

---
+++ FitnessTrackerApplication/View Controllers/HomeController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeController.swift
+//  FitnessTrackerApplication
+//
+//  Created by Xcode User on 2018-03-26.
+//  Copyright © 2018 RADS. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
