76ea748ef3875cc83ab533899b45323d03383284
Raj Patel <raj312.raj312@gmail.com>
2018-04-02 21:21:02 -0400

    Retrieve some data from healthkit and display it to the user to prove the concept

--- FitnessTrackerApplication/View Controllers/TodayController.swift
+++ FitnessTrackerApplication/View Controllers/TodayController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import HealthKit
+
+let healthkitStore:HKHealthStore = HKHealthStore()
 
 class TodayController: UIViewController {
-
+    
+    @IBOutlet var lbAge: UILabel!
+    @IBOutlet var lbBloodType: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //run authorize healthkit code once the view loads
+        self.authorizeHealthKit()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +29,56 @@ class TodayController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Healthkit methods
+    func authorizeHealthKit(){
+        
+        let healthKitTypesRead : Set<HKObjectType> = [
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+            HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!
+            //HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.appleExerciseTime)!
+        ]
+        let healthKitTypesWrite : Set<HKSampleType> = []
+        
+        if !HKHealthStore.isHealthDataAvailable() {
+            print("Error occured")
+            return
+        }
+        
+        healthkitStore.requestAuthorization(toShare: healthKitTypesWrite, read: healthKitTypesRead) {
+            (success, error) -> Void in
+            print("Read Write Authorisation succeeded")
+            
+        }
+    }
+    
+    func readFromHealthKit() -> (age: Int?, bloodType: HKBloodTypeObject){
+        var age: Int?
+        var bloodType: HKBloodTypeObject?
+        // var exerciseTime: Int?
+        
+        //calculate age
+        do {
+            let birthDay = try healthkitStore.dateOfBirthComponents()
+            let calendar = Calendar.current
+            let currentYear = calendar.component(.year, from: Date())
+            age = currentYear - birthDay.year!
+        }catch{}
+        
+        //read blood type
+        do {
+            bloodType = try healthkitStore.bloodType()
+        } catch{}
+        
+        return (age, bloodType!)
+    }
 
+    @IBAction func getDetails(sender: UIButton) {
+        let (age, bloodType) = readFromHealthKit()
+        self.lbAge.text = "Age: \(age ?? 0) years"
+        self.lbBloodType.text = "Blood Type: bloodType"
+        //need to write a method (switch statement) to convert bloodtype object to values like A+, etc.
+    }
+    
     /*
     // MARK: - Navigation
 
