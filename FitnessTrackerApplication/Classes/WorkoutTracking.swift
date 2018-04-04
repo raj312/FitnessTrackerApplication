4094abbc3d8ab5ca398b7c371ee9c03f539120ed
Anthony Rella <anthonyrella@outlook.com>
2018-04-04 17:07:40 -0400

    Track Progress First Draft

--- FitnessTrackerApplication/Classes/WorkoutTracking.swift
+++ FitnessTrackerApplication/Classes/WorkoutTracking.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2018 RADS. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class WorkoutTracking: NSObject {
+
+    var workoutName: String = "Bench Press"
+    var youtubeVideo: String = "https://www.youtube.com/embed/gRVjAtPip0Y?ecver=2"
+    var reps: Double?
+    var sets: Double?
+    var weight: Double?
+    var duration: Double?
+    
+    func saveData(reps : Double, sets: Double, weight: Double, duration: Double ){
+        
+        self.reps = reps
+        self.sets = sets
+        self.weight = weight
+        self.duration = duration
+    }
+    
+    func writeToDB()
+    {
+        print(self.reps, self.sets, self.weight, self.duration)
+    }
+    
+}
+
--- FitnessTrackerApplication/View Controllers/TrackController.swift
+++ FitnessTrackerApplication/View Controllers/TrackController.swift
@@ -7,13 +7,80 @@
 //
 
 import UIKit
+import WebKit
 
-class TrackController: UIViewController {
+class TrackController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet var workoutNameLabel: UILabel!
+    @IBOutlet var repsValue: UILabel!
+    @IBOutlet var setsValue: UILabel!
+    @IBOutlet var weightValue: UILabel!
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet var activity: UIActivityIndicatorView!
+    @IBOutlet var time: UIDatePicker!
+    
+   
+    @IBAction func repStepper(sender: UIStepper!){
+        repsValue.text = String(Int(sender.value))
+    }
+    
+    @IBAction func setStepper(sender: UIStepper!){
+       
+        setsValue.text = String(Int(sender.value))
+    }
+    @IBAction func weightStepper(sender: UIStepper!){
+        weightValue.text = String(Int(sender.value))
+    }
+
+    @IBAction func saveDateButton(sender : UIButton!){
+        
+        let alert = UIAlertController(title: "Save Progress", message: "Do you wish to save your progress? ", preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .default, handler:
+    
+            { (alert : UIAlertAction!) in
+                self.doTheUpdate()
+        }
+        )
+        
+        let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        
+        alert.addAction(yesAction)
+        alert.addAction(noAction)
+        
+        present(alert, animated: true)
+    }
+    
+    func doTheUpdate(){
+         let workOutTracking : WorkoutTracking = .init()
+         workOutTracking.saveData(reps: Double(repsValue.text!)!, sets: Double(setsValue.text!)!, weight: Double(weightValue.text!)!, duration: time.countDownDuration)
+        workOutTracking.writeToDB()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        let workOutTracking : WorkoutTracking = .init()
+        workoutNameLabel.text = workOutTracking.workoutName
+
+       let urlAddress = URL(string: workOutTracking.youtubeVideo)
+       let url = URLRequest(url: urlAddress!)
+       webView.load(url as URLRequest)
+       webView.navigationDelegate = self
+    }
+    
+ 
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+
+        activity.startAnimating()
+        activity.isHidden = false
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+
+        activity.stopAnimating()
+        activity.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
