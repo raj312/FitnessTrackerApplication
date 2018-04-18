beca49c3797ca1608d1a4edcc255615b360648b1
DeJoun Robinson <xcode@fasts-macbook-pro.local>
2018-04-18 14:39:29 -0400

    Added FMDB Wrapper

---
+++ FitnessTrackerApplication/DAO/DBManager.swift
@@ -0,0 +1,14 @@
+//
+//  DBManager.swift
+//  FitnessTrackerApplication
+//
+//  Created by DeJoun Robinson on 2018-04-18.
+//  Copyright © 2018 RADS. All rights reserved.
+//
+
+import UIKit
+
+class DBManager: NSObject {
+    static let shared: DBManager = DBManager()
+    
+}
